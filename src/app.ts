02d1f50c51b5d3b8a9f52fa2e2117fd1ba4635b1
Victor <vanmatos@poli.ufrj.br>
2022-10-22 22:18:00 -0300

    feat: get ticket categories

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { loadEnv, connectDb, disconnectDB } from '@/config';
 loadEnv();
 
 import { handleApplicationErrors } from '@/middlewares';
-import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter } from '@/routers';
+import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter, ticketsRouter } from '@/routers';
 
 const app = express();
 app
@@ -19,6 +19,7 @@ app
   .use('/auth', authenticationRouter)
   .use('/event', eventsRouter)
   .use('/enrollments', enrollmentsRouter)
+  .use('/tickets', ticketsRouter)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,3 +2,4 @@ export * from './users-router';
 export * from './authentication-router';
 export * from './events-router';
 export * from './enrollments-router';
+export * from './tickets-router';
